7438580eeb4efce63e23b2848ec33d17e72dd92b
Jake <jake@archive.org>
2014-06-05 12:58:57 -0700

    Use gevent to count individual indexes asynchronously

--- scripts/add_imagecount_to_item.py
+++ scripts/add_imagecount_to_item.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+import gevent.monkey
+gevent.monkey.patch_all()
+import gevent.pool
+import gevent.queue
+
 import sys
 import io
 import gzip
@@ -7,20 +12,42 @@ import requests
 from internetarchive import get_item
 
 
+Q = gevent.queue.Queue()
+
+
 def get_gzip_file_from_url(url):
     r = requests.get(url)
     bi = io.BytesIO(r.content)
-    return gzip.GzipFile(fileobj=bi, mode='rb')
+    gf = gzip.GzipFile(fileobj=bi, mode='rb')
+    count = 0
+    count += (len([x for x in gf if x]) - 1)
+    Q.put(count)
 
+def get_index_urls(item):
+    for f in item.iter_files():
+        if f.format == 'Comma-Separated Values GZ':
+            yield 'http://archive.org/download/{0}/{1}'.format(item.identifier, f.name)
 
 if __name__ == '__main__':
     identifier = sys.argv[-1]
     item = get_item(identifier)
-    count = 0
-    for f in item.iter_files():
-        if f.format == 'Comma-Separated Values GZ':
-            gf = get_gzip_file_from_url(f.url)
-            count += len([x for x in gf if x]) - 1
-    md = dict(imagecount=count)
-    print item.metadata['metadata'].get('title')
-    print '{0}\t{1}'.format(item.identifier, item.modify_metadata(md).get('status_code'))
+
+    pool = gevent.pool.Pool(40)
+    pool.map(get_gzip_file_from_url, get_index_urls(item))
+    _imagecount = 0
+
+    while not Q.empty():
+        _imagecount += Q.get()
+
+    if _imagecount == int(item.metadata.get('imagecount', 0)):
+        sys.stdout.write('{0} - imagecount is up to date\n'.format(item.identifier))
+        sys.exit(0)
+
+    md = dict(imagecount=_imagecount)
+    r = item.modify_metadata(md)
+    if r.status_code == 200:
+        sys.stdout.write('{0} - imagecount is up to date\n'.format(item.identifier))
+        sys.exit(0)
+    else:
+        sys.stderr.write('{0} - error updating imagecount, {1}\n'.format(item.identifier, r.json().get('error')))
+        sys.exit(1)
